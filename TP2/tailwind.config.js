a7721b93df6ba110348558c8bcd103e70f7a9038
Edouard <edouard@voltajazz.com>
2024-04-30 10:32:19 +0200

    Début séance 4

--- TP2/tailwind.config.js
+++ TP2/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["index.html", "./src/html/*.html"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["index.html", "./src/html/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          50: "#f5f7fa",
+          100: "#e9eef5",
+          200: "#cedae9",
+          300: "#a3bad6",
+          400: "#7297be",
+          500: "#507aa7",
+          600: "#3d618c",
+          700: "#334e71",
+          800: "#2d445f",
+          900: "#2a3a50",
+          950: "#1e293a",
+        },
+      },
+    },
+  },
+  plugins: [],
 };
